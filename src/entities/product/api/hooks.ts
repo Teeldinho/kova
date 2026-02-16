df42ec956b97b3f9da08f4547a10c7e4c38c2dcf
Tshepang <tshepangmog10@gmail.com>
2026-02-16 18:04:43 +0200

    feat(product): create product query options and wrapper hooks
    - productQueries with queryOptions pattern for list and detail
    - select transforms to unwrap API response (data field extraction)
    - useProducts and useProduct suspense hooks for components
    - Public API barrel export for product entity
    - 5-minute staleTime to prevent redundant refetches

---
+++ src/entities/product/api/hooks.ts
@@ -0,0 +1,8 @@
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+import { productQueries } from "./queries";
+
+export const useProducts = () => useSuspenseQuery(productQueries.list());
+
+export const useProduct = (id: number) =>
+  useSuspenseQuery(productQueries.detail(id));
---
+++ src/entities/product/index.ts
@@ -0,0 +1,4 @@
+export { productQueries } from "./api/queries";
+export { useProduct, useProducts } from "./api/hooks";
+export type { Product, ProductCategory, ProductRating } from "./model/types";
+export { PRODUCT, PRODUCT_CATEGORIES } from "./config/constants";
